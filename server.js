c54b0cda15da41027c0c82297b70608fa5883ce2
phisham <84333011+phisham@users.noreply.github.com>
2021-06-30 23:52:36 -0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,32 @@
+const express=require("express");
+const bodyParser=require("body-parser");
+const app=express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.get("/",function(req,res){
+    res.sendFile(__dirname+"/index.html")
+})
+app.post("/",function(req,res){
+    var n1=req.body.lcm1;
+    var n2=req.body.lcm2;
+    var c=0;
+    for(var i=1;i<=n2;i++){
+        var co_mult1=n1*i;
+        for(var j=1;j<=n1;j++){
+            var co_mult2=n2*j;
+            if(co_mult1==co_mult2){
+                res.send("LCM IS : "+co_mult2);
+                c=1;
+                break;
+            }
+        }
+        if(c==1){
+            break;
+        }
+    }
+    
+
+})
+
+app.listen(3000,function(){
+    console.log("server has started on port 3000")
+})
